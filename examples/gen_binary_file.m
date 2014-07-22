c7ddf225e5f72cd01ed248148b9d044ca7bcab82
Ruoxi Wang <ruoxi@stanford.edu>
2014-07-22 13:15:52 -0700

    add comment

--- examples/gen_binary_file.m
+++ examples/gen_binary_file.m
@@ -1,15 +1,17 @@
-% generate binary file for BBFMM3D
-Ns = 1e4;
-Nf = 1e4;
-L = 1;
-dof_s = 9;
+% Example of generating binary file for BBFMM3D
+
+Ns = 1e4; % Number of sources in simulation cell
+Nf = 1e4; % Number of field points in simulation cell
+L = 1;    % Length of simulation cell (assumed to be a cube)
+dof_s = 9;% Dimension of the tensor kernel
 dof_f = 6;
-m = 1;
-source = (rand(Ns,3) - 0.5) .* L;
-field = (rand(Nf,3) - 0.5) .* L;
-q = rand(Ns * dof_s * m,1);
+m = 1;    % Number of columns of the vector
+source = (rand(Ns,3) - 0.5) .* L; % Positions of source points
+field = (rand(Nf,3) - 0.5) .* L;  % Positions of field points
+q = rand(Ns * dof_s * m,1);       % charge
+
 source_binary = reshape(source,Ns * 3,1);
-field_binary = reshape(field,Nf*3,1);
+field_binary = reshape(field,Nf * 3,1);
 
 fid = fopen('source1e4_test.bin', 'w');
 fwrite(fid, source_binary, 'double');
